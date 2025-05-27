260e9380e2f1e2f0e3b76a085031fa25c7d8cb5f
danh0999 <dangducanh999@gmail.com>
2025-05-27 14:33:35 +0700

    config alias

--- Blood_Donation/vite.config.js
+++ Blood_Donation/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sassDts from "vite-plugin-sass-dts";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), sassDts()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@components": resolve(__dirname, "src/components"),
+      "@styles": resolve(__dirname, "src/assets/styles"),
+      "@icon": resolve(__dirname, "src/assets/Icons"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@context": resolve(__dirname, "src/context"),
+    },
+  },
+});
