e9597403e414372563e647908fe3d2474461319d
ngotuanphong <phongntse171994@fpt.edu.vn>
2025-06-16 11:23:24 +0700

    update-mainlayout-route

--- Blood_Donation/src/routes/ProtectedRoute.jsx
+++ Blood_Donation/src/routes/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
-const ProtectedRoute = ({ allowedRoles }) => {
+const ProtectedRoute = () => {
   // const user = useSelector((state) => state.user);
 
-  // if (!user) {
+  // if (!user || !user.token) {
   //   return <Navigate to="/login" replace />;
   // }
 
-  // if (allowedRoles && !allowedRoles.includes(user.role)) {
+  // if (!allowedRoles.includes(user.role)) {
   //   return <Navigate to="/" replace />;
   // }
 
