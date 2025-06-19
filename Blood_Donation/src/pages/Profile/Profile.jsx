42175dc1d37626bceb4f2e1f92b00c88a05492d4
ngotuanphong <phongntse171994@fpt.edu.vn>
2025-06-19 10:47:38 +0700

    update-profile-blood

--- Blood_Donation/src/pages/Profile/Profile.jsx
+++ Blood_Donation/src/pages/Profile/Profile.jsx
@@ -14,14 +14,13 @@ const Profile = () => {
   const [isUpdateModalVisible, setUpdateModalVisible] = useState(false);
 
   const bloodTypeOptions = [
-    { label: "A+", value: "A_POSITIVE" },
-    { label: "A-", value: "A_NEGATIVE" },
-    { label: "B+", value: "B_POSITIVE" },
-    { label: "B-", value: "B_NEGATIVE" },
-    { label: "AB+", value: "AB_POSITIVE" },
-    { label: "AB-", value: "AB_NEGATIVE" },
-    { label: "O+", value: "O_POSITIVE" },
-    { label: "O-", value: "O_NEGATIVE" },
+    { label: "A", value: "A" },
+
+    { label: "B", value: "B" },
+
+    { label: "AB", value: "AB" },
+
+    { label: "O", value: "O" },
   ];
 
   const [form] = Form.useForm();
