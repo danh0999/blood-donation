ef20e8bfe0d82bc54868e10a524a776d88e281de
QuangKhanh34th <quangkhanhktdn@gmail.com>
2025-07-16 22:33:29 +0700

    config backend production url

--- Blood_Donation/src/configs/axios.js
+++ Blood_Donation/src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://ougon-no-chi-abbd71acd669.herokuapp.com/api/",
 });
 
 api.interceptors.request.use(
